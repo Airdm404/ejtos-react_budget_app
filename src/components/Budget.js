66797b090f7e4fd54eab917fbabac6ab12b0e259
Airdm404 <edemcapp101@gmail.com>
2023-11-22 17:49:53 -0500

    budget is editable; upper limit set

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -2,17 +2,38 @@ import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
 
 const Budget = () => {
-    const { budget } = useContext(AppContext);
+    const { budget, dispatch } = useContext(AppContext);
     const [newBudget, setNewBudget] = useState(budget);
 
     const handleBudgetChange = (event) => {
+        if (event.target.value > 20000) {
+            alert("The value cannot exceed remaining funds  £"+20000);
+            return;
+        }
+
         setNewBudget(event.target.value);
     }
 
+    const handleKeyPress = (event) => {
+        if (event.key === "Enter") {
+            dispatch({
+                type: "SET_BUDGET",
+                payload: newBudget,
+            });
+        }
+
+    };
+
     return (
         <div className='alert alert-secondary'>
-        <span>Budget: £{budget}</span>
-        <input type="number" step="10" value={newBudget} onChange={handleBudgetChange}></input>
+        <span>Budget: £</span>
+        <input 
+            type="number" 
+            step="10" 
+            value={newBudget} 
+            onChange={handleBudgetChange} 
+            onKeyPress = {handleKeyPress}
+        />
         </div>
     );
 };
